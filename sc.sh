95fc9ec8b2253702d861c44a836faabc2227d629
Korgan Rivera <korganrivera@gmail.com>
2019-06-04 09:55:20 -0500

    gnuplot script and copy tss.log to dropbox

--- sc.sh
+++ sc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /home/korgan/code/silvercheetah/silvercheetah
 sudo -u korgan DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "SILVERCHEETAH" "I am the cheetah!" --icon=starred
+cp ~/code/silvercheetah/tss.log ~/Dropbox/tss.log
